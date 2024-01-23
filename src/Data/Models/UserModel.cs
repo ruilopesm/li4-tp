f29646e2a9bf9465996354173322f4fcf9e94282
chico ferreira <36338391+chicoferreira@users.noreply.github.com>
2024-01-23 14:43:05 +0000

    Revert "diagrams: Refactored Usecase Diagrams and made Component Diagram" (#7)

--- src/Data/Models/UserModel.cs
+++ src/Data/Models/UserModel.cs
@@ -5,8 +5,7 @@ namespace OnlineAuctions.Data.Models
         public int ID { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        // TODO: Should be an hash
-        public string Password { get; set; }
+        public string PasswordHash { get; set; }
         public Role Role { get; set; }
         public string RoleString => Role.ToString();
     }
