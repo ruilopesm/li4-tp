09ca08574943bd512c09939bd0e98ffe486cafbb
Daniel Pereira <46000449+danielsp45@users.noreply.github.com>
2024-01-30 21:11:56 +0000

    feat: create auction (#10)
    Co-authored-by: Rui Lopes <ruilopesm19@gmail.com>

--- src/Data/Services/AuctionService.cs
+++ src/Data/Services/AuctionService.cs
@@ -154,6 +154,24 @@ namespace OnlineAuctions.Data.Services
             return data.FirstOrDefault();
         }
 
+        public async Task<AuctionModel> CreateAuction(int productId, DateTime start, DateTime end, decimal startPrice, int publisherId)
+        {
+            const string sql = 
+                @"INSERT INTO dbo.Auction (ProductID, Start, [End], StartPrice, CurrentPrice, PublisherID) VALUES (@ProductID, @Start, @End, @StartPrice, @CurrentPrice, @PublisherID)";
+
+            var id = await _db.Connection.ExecuteScalarAsync<int>(sql, new
+            {
+                ProductID = productId,
+                Start = start,
+                End = end,
+                StartPrice = startPrice,
+                CurrentPrice = startPrice,
+                PublisherID = publisherId
+            });
+
+            return (await GetAuction(id))!;
+        }
+
         public async Task<bool> HasAdminCreatedAnyAuction(int adminId)
         {
             const string sql = @"SELECT * FROM dbo.Auction WHERE PublisherID = @ID";
